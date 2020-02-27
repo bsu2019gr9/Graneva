da0ebe6bd56cedb7e5bd1d64608f3d1a0ebcc632
granevaanna <59254291+granevaanna@users.noreply.github.com>
2020-02-27 14:13:57 +0300

    27-02-2020

---
+++ fuctionsFor1DArrays/Arrays-1Dem.h
@@ -0,0 +1,17 @@
+#pragma once
+void mkArrOfInt(int, int*&);
+void freeMemory(int*&);
+void printArr(int*, int*);
+void initArray(int*, int*);
+void initArrayRand(int*, int*, const int min = -10, const int max = 10);
+void inputArray(int*, int*);
+int findMax(int*, int*);
+int findMin(int*, int*);
+int sumArray(int*, int*);
+void countpositiveNegative(int*, int*);
+void reverse(int*, int*);
+void bubbleSort(int*, int*);
+void insertionSort(int*, int*);
+void gnomeSort(int*, int*);
+void shiftLeft(int*, int*);
+void shifRight(int*, int*);
